19a5fd3103f993570949728c400e8bc77c618065
MN Industries <130522379+FoundationINCCorporateTeam@users.noreply.github.com>
2024-05-21 18:48:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,55 @@
 import gradio as gr
-import requests
+from huggingface_hub import InferenceClient
 
-API_URL = "/api/respond"
+"""
+For more information on `huggingface_hub` Inference API support, please check the docs: https://huggingface.co/docs/huggingface_hub/v0.22.2/en/guides/inference
+"""
+client = InferenceClient("HuggingFaceH4/zephyr-7b-beta")
 
-def call_respond(message, history, system_message, max_tokens, temperature, top_p):
-    response = requests.post(
-        API_URL,
-        json={
-            "message": message,
-            "history": history,
-            "system_message": system_message,
-            "max_tokens": max_tokens,
-            "temperature": temperature,
-            "top_p": top_p,
-        },
-    )
-    return response.json()["response"]
 
-def respond(message, history, system_message, max_tokens, temperature, top_p):
-    return call_respond(message, history, system_message, max_tokens, temperature, top_p)
+def respond(
+    message,
+    history: list[tuple[str, str]],
+    system_message,
+    max_tokens,
+    temperature,
+    top_p,
+):
+    messages = [{"role": "system", "content": system_message}]
 
+    for val in history:
+        if val[0]:
+            messages.append({"role": "user", "content": val[0]})
+        if val[1]:
+            messages.append({"role": "assistant", "content": val[1]})
+
+    messages.append({"role": "user", "content": message})
+
+    response = ""
+
+    for message in client.chat_completion(
+        messages,
+        max_tokens=max_tokens,
+        stream=True,
+        temperature=temperature,
+        top_p=top_p,
+    ):
+        token = message.choices[0].delta.content
+
+        response += token
+        yield response
+
+"""
+For information on how to customize the ChatInterface, peruse the gradio docs: https://www.gradio.app/docs/chatinterface
+"""
 demo = gr.ChatInterface(
     respond,
-    additional_inputs=[
+        additional_inputs=[
         gr.Textbox(value="You are a friendly Chatbot.", label="System message"),
         gr.Slider(minimum=1, maximum=2048, value=512, step=1, label="Max new tokens"),
     ],
 )
 
+
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    demo.launch()
